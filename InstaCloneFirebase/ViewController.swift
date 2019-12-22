550ae0d1047e206e51fe78cd002820df09b4e6a4
Marcus Vinicius Galdino Medeiros <tinuthas@Marcuss-MacBook-Pro.local>
2019-12-22 00:04:15 -0300

    create project

---
+++ InstaCloneFirebase/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InstaCloneFirebase
+//
+//  Created by Marcus Vinicius Galdino Medeiros on 21/12/19.
+//  Copyright © 2019 Marcus Vinicius Galdino Medeiros. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
