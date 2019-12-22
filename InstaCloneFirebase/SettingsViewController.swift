9d1a1d59eb3d3b369eca2da4254f9c80a84aca3f
Marcus Vinicius Galdino Medeiros <tinuthas@Marcuss-MacBook-Pro.local>
2019-12-22 12:53:44 -0300

    settings view and logout application

---
+++ InstaCloneFirebase/SettingsViewController.swift
@@ -0,0 +1,23 @@
+//
+//  SettingsViewController.swift
+//  InstaCloneFirebase
+//
+//  Created by Marcus Vinicius Galdino Medeiros on 22/12/19.
+//  Copyright © 2019 Marcus Vinicius Galdino Medeiros. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func logoutClicked(_ sender: Any) {
+        performSegue(withIdentifier: "toViewController", sender: nil)
+    }
+
+}
